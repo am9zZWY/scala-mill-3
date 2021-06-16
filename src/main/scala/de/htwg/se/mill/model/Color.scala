f0aad0493edc692707e79dec7e0ec558f8a8dbe7
jpkmiller <j.mueller5021@gmail.com>
2021-06-16 09:32:24 +0200

    Color: new enum syntax.

--- src/main/scala/de/htwg/se/mill/model/Color.scala
+++ src/main/scala/de/htwg/se/mill/model/Color.scala
@@ -1,8 +1,4 @@
 package de.htwg.se.mill.model
 
-case object Color extends Enumeration {
-  val white = Value
-  val black = Value
-  val noColor = Value
-}
-
+enum Color:
+  case white, black, noColor
