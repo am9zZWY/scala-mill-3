a37b0ddda27d9089afa9574aa7af20bc7eb973c6
jpkmiller <j.mueller5021@gmail.com>
2021-06-16 09:32:14 +0200

    MillState: 2 -> 3.

--- src/main/scala/de/htwg/se/mill/model/MillState.scala
+++ src/main/scala/de/htwg/se/mill/model/MillState.scala
@@ -1,35 +1,28 @@
 package de.htwg.se.mill.model
 
-trait MillState {
+trait MillState:
   def handle: String
-}
 
-case class WhiteMillState() extends MillState {
+case class WhiteMillState() extends MillState:
   override def handle: String = "White Mill"
-}
 
-case class BlackMillState() extends MillState {
+case class BlackMillState() extends MillState:
   override def handle: String = "Black Mill"
-}
 
-case class NoMillState() extends MillState {
+case class NoMillState() extends MillState:
   override def handle: String = "No Mill"
-}
 
-object MillState {
-  def handle(e: MillState): String = {
-    e match {
-      case WhiteMillState() => WhiteMillState().handle
-      case BlackMillState() => BlackMillState().handle
-      case _ => NoMillState().handle
-    }
-  }
+object MillState:
+  def handle(e: MillState): String =
+    (e match
+      case WhiteMillState() => WhiteMillState()
+      case BlackMillState() => BlackMillState()
+      case _ => NoMillState()
+    ).handle
 
-  def whichState(s: String): MillState = {
-    s match {
+  def whichState(s: String): MillState =
+    s match
       case "White Mill" => WhiteMillState()
       case "Black Mill" => BlackMillState()
-      case "No Mill" => NoMillState()
-    }
-  }
-}
+      case _ => NoMillState()
+  
